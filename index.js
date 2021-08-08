d1c30b8da15898755a602d20ccf48c4a110506ef
chunkbanned <59180376+chunkbanned@users.noreply.github.com>
2021-08-08 12:22:49 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const { MessageEmbed } = require('discord.js');
 const { Client, Intents } = require('discord.js');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
-const BannedWords = ["admin abuse", "aa", "adminabuse", "admin abooz", "admin aboz", "adin abooz", "admin adbusese", "admin abuser", "admin abusing", "Admin abusing", "Admin abuse", "Admin Abuse", "Adminabuse", "AdminAbuse", "admin Abuse", "adminAbuse", "aa", "Aa", "aA", "adminabuse", "admin abooz", "Admin aboz", "adminabooz", "adin abooz", "admin adbusese", "admin abuser", "AAing", "adminn abooze", "admin abuze", "cunt", "nigger", "nigga", "faggot", "slut", "queer", "tranny", "trannies", "whore", "anal", "anus", "arse", "sex", "raid", "masturbate", "pussy", "penis", "cum", "sperm", "semen", "clitoris", "dick", "dildo", "tits", "slave", "pornhub", "porn", "kys", "fag", "stearn", "cs:go", "fuck", "hentai", "fucker", "cervix", "bitch", "prison smp", "prison rp", "discord.gg", "discord.io", "hurricane chaser chase"]
+const BannedWords = [""]
 const warn = new MessageEmbed()
 	.setColor('#FF0000')
 	.setTitle('Blacklisted Word Detected')
@@ -35,4 +35,4 @@ client.on('interactionCreate', async interaction => {
 client.on("message", message => {
     if (BannedWords.some(word => message.toString().toLowerCase().includes(word))) {message.delete().catch(e => console.error("e")); message.reply({ embeds: [warn] }); message.channel.send('<@&873632222907859004>')};
 });
-client.login('ODU2OTc5MTUyNTIzODg2NjAy.YNI6Bw.QFc4C89aO4FUpBCL_V-Ue2sfS8Q');
+client.login('');
